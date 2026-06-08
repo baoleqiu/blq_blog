c9b33f6ce76891c6315fdc959bfcfeb1735f0e81
blq <3065864388@qq.com>
2026-06-08 16:51:42 +0800

    禁用社交图标旋转动画

--- scripts/inject-css.js
+++ scripts/inject-css.js
@@ -1,6 +1,7 @@
 hexo.extend.injector.register('head_end', `<style>
-/* 禁用头像旋转动画 */
-.avatar-img img:hover {
+/* 禁用头像和社交图标旋转动画 */
+.avatar-img img:hover,
+.social-icon:hover {
   transform: none !important;
 }
 </style>`);
